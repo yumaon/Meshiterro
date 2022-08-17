48d83e70f361d8a20df7ad76d8e52ddb8bf4c8f4
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-17 16:14:02 +0000

    [Finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # ↑こう記述することで、devise利用の機能が使われる前にconfigure_permitted_patametersメソッドが実行される。
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使う事で
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータの操作を許可している。
+  # ストロングパラメータと同様の機能である。
+  # privateは記述したコントローラ内でしか参照できないが、protectedは呼び出された他のコントローラからも参照できる。
 end
