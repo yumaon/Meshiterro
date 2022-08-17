686bb495752020ba30d8f27434c3a4f6f07f8671
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-17 17:04:12 +0000

    [Finish]4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # ↑こう記述することで、devise利用の機能が使われる前にconfigure_permitted_patametersメソッドが実行される。
 
+  # ↓ after_sign_in_path_forはDeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定しているメソッド。
+  # after_sign_in_path_forは、初期設定ではroot_pathになっている。
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  # ↓ after_sign_out_path_forはDeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定しているメソッド。
+  # after_sign_out_path_forは、初期設定ではroot_pathになっている。
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   protected
 
   def configure_permitted_parameters
