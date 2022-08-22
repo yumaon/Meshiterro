96f4a0f28569fbde0b752a9db05152a4ce794aa5
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-22 02:24:03 +0000

    [Finish]4-24 FontAwesomeの導入まで

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $:'jquery/syc/jquery',
+    jQuery:'jquery/syc/jquery',
+    Popper:'popper.js'
+  })
+)
